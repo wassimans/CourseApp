69b5c2d5fcb4b702b2a3c58a88fa714e18f49723
Wassim MANSOURI <wassim.mansouri@redfabriq.com>
2021-02-18 08:25:07 +0100

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Wassim Mansouri on 18/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
